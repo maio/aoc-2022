37be82d0405d0da2dc8b9dd1370bd1662c697106
Marian Schubert <marian.schubert@gmail.com>
2022-12-04 15:56:33 +0100

    Implement readResource utility function

--- shared/src/main/kotlin/cz/maio/Utils.kt
+++ shared/src/main/kotlin/cz/maio/Utils.kt
@@ -1,1 +1,8 @@
 package cz.maio
+
+object Utils {}
+
+fun readResource(name: String): String {
+    val resource = Utils.javaClass.getResource(name) ?: error("Resource $name not found")
+    return resource.readText()
+}
